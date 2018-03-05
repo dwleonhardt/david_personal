fd8c717424958f6397f64b902d1f4f1e4e34a25c
dwleonhardt <dwleonhardt@gmail.com>
2018-03-05 15:09:33 -0700

    zoom working

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,43 +5,67 @@ import { TweenLite, TimelineLite } from 'gsap';
 
 
 let scene = new THREE.Scene();
+// scene.background = new THREE.Color( 0xffffff,0 );
 
 let camera = new THREE.PerspectiveCamera(75,window.innerWidth/window.innerHeight,1,1000);
+camera.position.z = 100;
 
 let renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
-renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.setPixelRatio((window.devicePixelRatio) ? window.devicePixelRatio : 1);
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.autoClear = false;
+renderer.setClearColor(0x000000, 0.0);
 document.body.appendChild( renderer.domElement );
 
-renderer.shadowMap.enabled = true;
-renderer.shadowMap.type = THREE.PCFSoftShadowMap; // default THREE.PCFShadowMap
-renderer.setClearColor (0x000000, 1);
-
 //Create a PointLight and turn on shadows for the light
-var light = new THREE.SpotLight( 0xffffff, 8, 90, 2 );
-light.position.set( 10, 20, 80 );
-light.castShadow = true;            // default false
-scene.add( light );
+var ambientLight = new THREE.AmbientLight(0x999999 );
+    scene.add(ambientLight);
 
-//Set up shadow properties for the light
-light.shadow.mapSize.width = 512;  // default
-light.shadow.mapSize.height = 512; // default
-light.shadow.camera.near = 0.5;       // default
-light.shadow.camera.far = 500      // default
-// var helper = new THREE.CameraHelper( light.shadow.camera );
-// scene.add( helper );
+    var lights = [];
+    lights[0] = new THREE.DirectionalLight( 0xffffff, 1 );
+    lights[0].position.set( 1, 0, 0 );
+    lights[1] = new THREE.DirectionalLight( 0x11E8BB, 1 );
+    lights[1].position.set( 0.75, 1, 0.5 );
+    lights[2] = new THREE.DirectionalLight( 0x8200C9, 1 );
+    lights[2].position.set( -0.75, -1, 0.5 );
+    scene.add( lights[0] );
+    scene.add( lights[1] );
+    scene.add( lights[2] );
 
-let geometry = new THREE.TetrahedronGeometry(1,2);
 
-let material = new THREE.MeshPhongMaterial({color:0x42f4cb, });
-let material1 = new THREE.MeshPhongMaterial({color:0x419bf4 });
+    window.addEventListener('resize', onWindowResize, false);
+    function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+}
 
-material.shininess = 60;
+
+
+var dotMat = new THREE.SpriteMaterial({
+	color: 0xffffff,
+
+});
+
+var dots = new THREE.Geometry();
+
+
+
+let geometry = new THREE.TetrahedronGeometry(4,1);
+
+
+let material = new THREE.MeshPhongMaterial({
+  color:0xffffff,
+  shading: THREE.FlatShading
+});
+
+
+material.shininess = 20;
 
 const cube = new THREE.Mesh(geometry,material);
 const cube1 = new THREE.Mesh(geometry,material);
 
 
-console.log(cube.rotation);
 
 cube.castShadow = true; //default is false
 cube.receiveShadow = true
@@ -51,37 +75,62 @@ cube1.receiveShadow = true
 scene.add(cube);
 scene.add(cube1);
 
-// let edges = new THREE.EdgesHelper( cube, 0x419bf4);
-// edges.material.linewidth = 2;
-// scene.add(edges);
-//
-// let edges1 = new THREE.EdgesHelper( cube1, 0x419bf4);
-// edges.material.linewidth = 2;
-// scene.add(edges1);
-// edges.castShadow = false; //default is false
-// edges.receiveShadow = true
-// edges1.castShadow = false; //default is false
-// edges1.receiveShadow = true
+var raycaster = new THREE.Raycaster();
+var mouse = new THREE.Vector2();
 
-camera.position.z = 5;
+window.addEventListener( 'mousemove', onMouseMove, false );
+function onMouseMove( event ) {
 
-// let degrees = 45;
-//
-// cube.rotation.y = degrees * (Math.PI/180);
-// cube.rotation.z = degrees * (Math.PI/180);
-// edges.rotation.y = degrees * (Math.PI/180);
-// edges.rotation.z = degrees * (Math.PI/180);
+  mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+  mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+  // console.log(cube.position.x === mouse.x);
+}
 
 
+
+
+
+
+let currentMesh;
+var fov = camera.fov, zoom = 1.0, inc = -0.007;
 
 function render (){
+  raycaster.setFromCamera( mouse, camera );
+
+  var intersects = raycaster.intersectObjects( scene.children );
+
+
+  camera.fov = fov * zoom;
+      camera.updateProjectionMatrix();
+
+      zoom += inc;
+      if ( zoom <= 0.2 || zoom >= 1.0 ){
+        inc = 0;
+        if (intersects.length > 0) {
+          if (intersects[0].object != currentMesh) {
+            currentMesh = intersects[0].object;
+            currentMesh.currentHex = currentMesh.material.color.getHex();
+            intersects[0].object.material.color.set( 0x42f4cb );
+          }
+        }
+         else if (intersects.length <= 1) {
+           if (currentMesh) {
+             currentMesh.material.color.set( 0xffffff )
+             currentMesh = null;
+           }
+         }
+       }
+
   requestAnimationFrame(render);
 
-  // camin();
   rotate();
 
   renderer.render(scene,camera);
 }
+
+
+
+
 render();
 
 function rotate(){
@@ -103,42 +152,14 @@ function rotate(){
 
 
 
-// let campo= true;
 
 
-// function camin(){
-//
-// if(campo==true){
-//     if(camera.position.z>2){
-//       camera.position.z-=.01;
-//     }
-//     else {
-//       campo = false;
-//       camout()
-//     }
-// }
-//
-// else if(campo==false){
-//   camout();
-//   console.log(camera.position.z);
-// }
-// }
-//
-// function camout(){
-//   if(camera.position.z<6){
-//     camera.position.z+=.1;
-//     console.log('going out')
-//   }
-//   else{
-//     campo=true;
-//   }
-// }
 
 
 class Home extends Component {
   render() {
     return (
-      <div className='header'>Header</div>
+      <div className='enter'></div>
     );
   }
 }
